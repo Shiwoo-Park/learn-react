bc108053f731ba71f2ad1d7550e4ac058ab5d319
silva.park <silva.park@sendbird.com>
2021-02-09 00:08:18 +0900

    learned react more !!!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import PropTypes from "prop-types";
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
   };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
   render() {
     const { isLoading } = this.state;
     return <div>{isLoading ? "Loading..." : "Loaded"}</div>;
